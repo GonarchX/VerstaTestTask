08f2619de6d37754b589189c18d370c4409ffa05
Valentin Pronin <valentinpronin@mail.ru>
2022-07-12 16:30:06 +0300

    Initialize "DeliveryOrderForm" model

---
+++ VerstaTestTask/Models/DeliveryOrderForm.cs
@@ -0,0 +1,26 @@
+﻿namespace VerstaTestTask.Models
+{
+    public class DeliveryOrderForm
+    {
+        public int Id { get; set; }
+        public string SenderCity { get; set; }
+        public string SenderAddress { get; set; }
+        public string RecipientCity { get; set; }
+        public string RecipientAddress { get; set; }
+        public float CargoWeight { get; set; }
+        public DateTime CargoPickupDate { get; set; }
+
+        public DeliveryOrderForm() { }
+
+        public DeliveryOrderForm(int id, string senderCity, string senderAddress, string recipientCity, string recipientAddress, float cargoWeight, DateTime cargoPickupDate)
+        {
+            Id = id;
+            SenderCity = senderCity;
+            SenderAddress = senderAddress;
+            RecipientCity = recipientCity;
+            RecipientAddress = recipientAddress;
+            CargoWeight = cargoWeight;
+            CargoPickupDate = cargoPickupDate;
+        }
+    }
+}
