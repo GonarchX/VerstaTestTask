914a0225a1f2a2e74964cf7d016ae79ba9246f4f
Valentin Pronin <valentinpronin@mail.ru>
2022-07-13 16:04:55 +0300

    Add DeletedDeliveryOrderInfo table

--- DatabaseWorker/Migrations/20220713130327_DeletedDeliveryOrderInfo.Designer.cs
+++ DatabaseWorker/Migrations/20220713130327_DeletedDeliveryOrderInfo.Designer.cs
@@ -4,6 +4,7 @@ using DatabaseWorker.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatabaseWorker.Migrations
 {
     [DbContext(typeof(VerstaTestTaskContext))]
-    partial class VerstaTestTaskContextModelSnapshot : ModelSnapshot
+    [Migration("20220713130327_DeletedDeliveryOrderInfo")]
+    partial class DeletedDeliveryOrderInfo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DatabaseWorker/Models/VerstaTestTaskContext.cs
+++ DatabaseWorker/Models/VerstaTestTaskContext.cs
@@ -4,6 +4,7 @@ namespace DatabaseWorker.Models
 {
     internal class VerstaTestTaskContext : DbContext
     {
+        public DbSet<DeletedDeliveryOrderInfo> DeletedDeliveryOrderInfo { get; set; } = null!;
         public DbSet<DeliveryOrder> DeliveryOrder { get; set; } = null!;
         public VerstaTestTaskContext(DbContextOptions<VerstaTestTaskContext> options) : base(options) { }
 
